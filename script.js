1c02e214c71443473b5498f69bab4f730212ebdf
Cy <aryssesanvicente@yahoo.com>
2022-12-28 22:37:23 +0800

    Add steps

---
+++ script.js
@@ -0,0 +1,10 @@
+// Create 16x16 grid as default on page load
+
+// Set default color of background
+
+// Set default color of ink
+
+// Change color of div on mouse click/hold
+
+// Add button to select custom color
+
