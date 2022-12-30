d16ba949492fbdf9bb53576a8537b1a44a835bcc
Cy <aryssesanvicente@yahoo.com>
2022-12-31 00:27:38 +0800

    Change buttons

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 
 // Set default color of background
 
+document.getElementById("background-color-picker").value = "#ffffff"
+
 // Set default color of ink
 
+document.getElementById("pen-color-picker").value = "#000000"
+
 // Change color of div on mouse click/hold
+
+
 
 // Add button to select custom color
 
