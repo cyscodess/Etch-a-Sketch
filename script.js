940c9bb4513e62221ec27a495fe1e98d2366b3de
Cy <aryssesanvicente@yahoo.com>
2023-01-01 23:46:30 +0800

    Add drawBoard function

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // Create 16x16 grid as default on page load
+const gridBoard = document.getElementById("grid-board")
+
+function drawBoard(size) {
+    gridBoard.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    gridBoard.style.gridAutoRows = `repeat(${size}, 1fr)`
+
+    for (let i = 0; i < size ** 2; i++) {
+        let cell = document.createElement("div")
+        gridBoard.appendChild(cell)
+    }
+}
+
+window.onload(drawBoard(16))
 
 // Set default color of background
 
